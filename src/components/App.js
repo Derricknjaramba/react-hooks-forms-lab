6881367a8955ab944989e45f45f24484487db4b1
Derricknjaramba <njarambaderrick@gmail.com>
2024-08-07 17:58:41 +0300

    forms

--- src/components/App.js
+++ src/components/App.js
@@ -8,15 +8,20 @@ function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   function handleDarkModeClick() {
-    setIsDarkMode((isDarkMode) => !isDarkMode);
+    setIsDarkMode(prevIsDarkMode => !prevIsDarkMode);
+  }
+
+  function handleItemFormSubmit(newItem) {
+    setItems([...items, newItem]);
   }
 
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} />
+      <ShoppingList items={items} onItemFormSubmit={handleItemFormSubmit} />
     </div>
   );
 }
 
 export default App;
+
